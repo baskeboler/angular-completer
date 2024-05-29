7edaf68bb9858120f4ad98b70928941c9ce29710
Victor Gil <2041346+baskeboler@users.noreply.github.com>
2024-05-29 08:12:53 +0000

    ng update angular 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NgxCompleterModule } from './modules/ngx-completer/ngx-completer.module';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    FormsModule,
-    BrowserModule,
-    HttpClientModule,
-    NgxCompleterModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent
+    ],
+    bootstrap: [AppComponent], imports: [FormsModule,
+        BrowserModule,
+        NgxCompleterModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
