3700bf05e05dcdfbf37ff05f34c66dd1011c07a3
Victor Gil <vgil@sportxapparel.com>
2024-05-31 13:31:17 -0300

    add CompleterService to public_api

--- public_api.ts
+++ public_api.ts
@@ -3,4 +3,5 @@ export * from './src/app/modules/ngx-completer/ngx-completer.module';
 export * from './src/app/modules/ngx-completer/services/data.service';
 export * from './src/app/modules/ngx-completer/services/remote-data.service';
 export * from './src/app/modules/ngx-completer/services/local-data.service';
+export * from './src/app/modules/ngx-completer/services/completer.service'
 export * from './src/app/modules/ngx-completer/model/completer-item';
